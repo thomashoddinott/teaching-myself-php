c7249a76e3f88238b7208152d12941c46ea79b35
Thomas Hoddinott <thomashoddinott477@gmail.com>
2020-08-05 19:02:02 +0100

    lesson 22 - importing in php

---
+++ 22_including_documents_in_php/header.php
@@ -0,0 +1,28 @@
+<?php
+  include 'functions/fn1.php'
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Title</title>
+<link rel="stylesheet" type="text/cs" href="style.css">
+</head>
+
+<body>
+<h3>The quick brown fox jumps over the lazy dog.<h3>
+<header>
+  <nav>
+    <ul>
+      <li><a href="">nav1</a></li>
+      <li><a href="">nav2</a></li>
+      <li><a href="">nav3</a></li>
+      <li><a href="">nav4</a></li>
+    </ul>
+  </nav>
+</header>
+
+
+
+</body>
---
+++ 22_including_documents_in_php/index.php
@@ -0,0 +1,19 @@
+<meta charset="UTF-8">
+<title>Title</title>
+</head>
+
+<body>
+
+<?php
+  include 'header.php';
+  foo()
+?>
+
+<section>
+  <div class="someMainContent">
+    <h1>Hi there!</h1>
+  </div>
+</section>
+
+</body>
+</html>  
