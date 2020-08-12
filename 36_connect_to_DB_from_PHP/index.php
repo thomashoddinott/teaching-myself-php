1dad5a1fe16dfd6029602229f9236c8a31bd6e42
Thomas Hoddinott <thomashoddinott477@gmail.com>
2020-08-12 15:43:12 +0100

    lesson 36

---
+++ 36_connect_to_DB_from_PHP/index.php
@@ -0,0 +1,17 @@
+<?php
+  include_once 'includes/dbh.inc.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Title</title>
+</head>
+<body>
+
+  <?php
+    //connect to database with $conn we've just included
+  ?>
+  
+</body>
+</html>
