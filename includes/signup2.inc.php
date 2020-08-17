c58a00245186a972c924c513019c7ecc7c4fe843
Thomas Hoddinott <thomashoddinott477@gmail.com>
2020-08-17 10:41:18 +0100

    added 2nd signup which uses prepared statements and made a few tweaks

--- includes/signup2.inc.php
+++ includes/signup2.inc.php
@@ -17,8 +17,18 @@ $pwd = mysqli_real_escape_string($conn, $_POST['pwd']);
 //prepared statements are the prefered way (according to this guy) to do database transactions from PHP
 
 $sql = "insert into users(user_first, user_last, user_email, user_uid, user_pwd)
-values ('$first', '$last', '$email', '$uid', '$pwd');";
-mysqli_query($conn, $sql);
+values (?, ?, ?, ?, ?);";
+$stmt = mysqli_stmt_init($conn);
+if (!mysqli_stmt_prepare($stmt, $sql)) {
+  echo "SQL error";
+} else {
+  mysqli_stmt_bind_param($stmt, "sssss", $first, $last, $email, $uid, $pwd);
+  mysqli_stmt_execute($stmt);
+}
 
-//fixme - should return to relative lesson folder, not 38 each time!
-header("Location: ../38_insert_to_DB_from_PHP/index.php?signup=success");
+//this will do for now
+if (!strpos($_SERVER['HTTP_REFERER'], "?signup=success")) {
+  header('Location: ' . $_SERVER['HTTP_REFERER'] . "?signup=success");
+} else {
+  header('Location: ' . $_SERVER['HTTP_REFERER']);
+}
