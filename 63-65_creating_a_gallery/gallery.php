178019759bb72408f7b9e739f37608fbca862c25
Thomas Hoddinott <thomashoddinott477@gmail.com>
2020-09-16 09:50:09 +0100

    lesson 63

---
+++ 63-65_creating_a_gallery/gallery.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Title</title>
+  </head>
+  <body>
+    <?php
+    ?>
+
+without css this is gonna look bad!
+
+    <main>
+      <section class="gallery-links">
+        <div class="wrapper">
+          <h2>Gallery</h2>
+          
+          <div class="gallery-container">
+            <a href="#">
+              <div></div>
+              <h3>h3 Title</h3>
+              <p>Paragraph</p>
+            </a>
+            <a href="#">
+              <div></div>
+              <h3>h3 Title</h3>
+              <p>Paragraph</p>
+            </a>
+            <a href="#">
+              <div></div>
+              <h3>h3 Title</h3>
+              <p>Paragraph</p>
+            </a>
+            <a href="#">
+              <div></div>
+              <h3>h3 Title</h3>
+              <p>Paragraph</p>
+            </a>
+            <a href="#">
+              <div></div>
+              <h3>h3 Title</h3>
+              <p>Paragraph</p>
+            </a>
+          </div>
+
+          <div class="gallery-upload">
+            <form action="includes/gallery-upload.inc.php" method="post" enctype="multipart/form-data">
+              <input type="text" name="filename" placeholder="File name">
+              <input type="text" name="filetitle" placeholder="Image title">
+              <input type="text" name="filedesc" placeholder="Image description">
+              <input type="file" name="file">
+              <button type="submit" name="submit">Upload</button>
+            </form>
+
+
+          </div>
+
+
+        </div> 
+      </section>
+    </main>
+  </body>
+</html> 
